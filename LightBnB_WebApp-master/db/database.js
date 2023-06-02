d9bd7597454193dca4cd19a9aacf1e5814a5d5cb
Patrick Tumu <patricktumu@gmail.com>
2023-06-01 20:17:29 -0600

    completed LighthouseBnB Add Property

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -58,9 +58,6 @@ const getUserWithId = function(id) {
 
 const addUser = function(user) {
 
-  console.log("password", user.password);
-  console.log("name", user.name);
-  console.log("email", user.email);
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
     //.query(`INSERT INTO users (name, email, password) VALUES ('patrick', 'u@u.com','password' )`)
@@ -160,10 +157,26 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryValues = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+  let queryString = `
+  INSERT INTO properties
+  (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `;
+
+  console.log(queryString, queryValues);
+  
+  return pool
+    .query(queryString, queryValues)
+    .then((result) => {
+      //console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports = {
@@ -174,11 +187,4 @@ module.exports = {
   getAllProperties,
   addProperty,
 };
-/*SELECT properties.*, avg(property_reviews.rating) as average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id 
-WHERE city LIKE '%vancouv%' AND cost_per_night > 10000 AND cost_per_night < 20000 AND rating > 2 
-GROUP BY properties.id
-ORDER BY cost_per_night
-LIMIT 5; 
-*/
+//VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;`
